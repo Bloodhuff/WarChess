64c9114172f43ba956a7cd15745d85b1d7465e32
Emil Łodygowski <burszuras1@gmail.com>
2015-06-12 20:45:16 +0200

    added grid and stuff

---
+++ WarChess/WarChess/Contract/Field.cs
@@ -0,0 +1,14 @@
+﻿namespace WarChess.Contract
+{
+	public class Field
+	{
+		public Field(short x, short y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public short X { get; set; } 
+		public short Y { get; set; } 
+	}
+}
